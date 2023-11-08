159888ef02f1fa45b8dda04cd70c4de95e7fa594
SAMFATEWOLF <s.v.bolonin@mail.ru>
2023-11-08 18:43:52 +0300

    Добавил регистрацию и авторизацию

--- djangosite/mysite/actors/urls.py
+++ djangosite/mysite/actors/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('about/', about, name='about'),
     path('addpage/', AddPage.as_view(), name='add_page'),
     path('contact/', contact, name='contact'),
-    path('login/', login, name='login'),
-    path('category/<int:cat_id>/', ShowCategory.as_view(), name='category')
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('category/<int:cat_id>/', ShowCategory.as_view(), name='category'),
+    path('register/', RegisterUser.as_view(), name='register')
 ]
--- djangosite/mysite/actors/views.py
+++ djangosite/mysite/actors/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import logout, login
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
@@ -39,14 +41,10 @@ def contact(request):
     return HttpResponse('contact')
 
 
-def login(request):
-    return HttpResponse('login')
-
-
 class AddPage(LoginRequiredMixin, DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'actors/addpage.html'
-    login_url = reverse_lazy('admin')  # TODO: отправить на авторизацию, вернуть через утилс
+    login_url = reverse_lazy('login')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -79,3 +77,37 @@ class ShowCategory(DataMixin, ListView):
         c_def = self.get_user_context(title='Категория - ' + str(context['posts'][0].cat),
                                       cat_selected=context['posts'][0].cat_id)
         return dict(list(context.items()) + list(c_def.items()))
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'actors/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'actors/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Авторизация')
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
